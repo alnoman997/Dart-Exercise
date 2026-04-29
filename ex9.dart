8d1ac870bfaa86076f49a84216f40ac31d7a8591
alnoman997 <alnoman997@gmail.com>
2026-04-29 22:39:46 +0600

    guessingGame

--- ex9.dart
+++ ex9.dart
@@ -1,4 +1,11 @@
-// Generate a random number between 1 and 100. 
-//Ask the user to guess the number, 
+// Generate a random number between 1 and 100.
+//Ask the user to guess the number,
 //then tell them whether they guessed too low, too high, or exactly right.
 
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print("Type 'exit' to quit the game.");
+  guessingGame();
+}

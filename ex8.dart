8c114f31904d4a56ea836f0d009a834386066baa
alnoman997 <alnoman997@gmail.com>
2026-04-29 22:05:22 +0600

    rock paper

--- ex8.dart
+++ ex8.dart
@@ -12,4 +12,21 @@ void main() {
     'scissors': 'paper',
     'paper': 'rock',
   };
+
+  int user = 0;
+  int comp = 0;
+
+  List<String> options = ['rock', 'paper', 'scissors'];
+
+  while (true) {
+    String compChoice = options[random.nextInt(options.length)];
+    stdout.write("\nEnter your choice (rock, paper, scissors): ");
+    String userChoice = stdin.readLineSync()!.toLowerCase();
+
+    if (userChoice == "exit") {
+      print("\nYou: $user, Computer: $comp");
+      print("Thanks for playing!");
+      break;
+    }
+  }
 }

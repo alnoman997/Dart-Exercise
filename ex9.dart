50b673791c36483f8ebd81bc82431c5213841464
alnoman997 <alnoman997@gmail.com>
2026-04-30 11:06:48 +0600

    random number

--- ex9.dart
+++ ex9.dart
@@ -1,6 +1,6 @@
 // Generate a random number between 1 and 100.
-//Ask the user to guess the number,
-//then tell them whether they guessed too low, too high, or exactly right.
+// Ask the user to guess the number,
+// then tell them whether they guessed too low, too high, or exactly right.
 
 import 'dart:io';
 import 'dart:math';
@@ -8,4 +8,16 @@ import 'dart:math';
 void main() {
   print("Type 'exit' to quit the game.");
   guessingGame();
+}
+
+guessingGame() {
+  final random = Random();
+  final randNumber = random.nextInt(100);
+  int attempt = 0;
+
+  while (true) {
+    attempt += 1;
+    stdout.write("Please chooose a number between 1 and 100: ");
+    String chosenNumber = stdin.readLineSync()!;
+  }
 }

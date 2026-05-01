b97a4020503a42052d61d3849836b7e8313bf774
alnoman997 <alnoman997@gmail.com>
2026-05-01 16:16:58 +0600

    random list

--- ex11.dart
+++ ex11.dart
@@ -1,6 +1,6 @@
 // Write a program that takes a list of numbers
 // and makes a new list of only the first and last elements of the given list.
-import 'dart:io';
+import 'dart:math';
 
 void main() {
   final random = Random();

d859ff30b1b49c3b0f0cf7635f2538e4cc8d0844
alnoman997 <alnoman997@gmail.com>
2026-05-01 08:35:25 +0600

    prime number

--- ex10.dart
+++ ex10.dart
@@ -1,3 +1,12 @@
-// Ask the user for a number 
+// Ask the user for a number
 // and determine whether the number is prime or not.
 
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write("Please enter a number: ");
+  int chosenNumber = int.parse(stdin.readLineSync()!);
+
+  checkPrime(chosenNumber);
+}

6172e4e2a2fd2b604c83d20f76d29f66ba0b077b
alnoman997 <alnoman997@gmail.com>
2026-05-02 08:52:29 +0600

    tic tac toe

--- ex20.dart
+++ ex20.dart
@@ -9,4 +9,21 @@ void main() {
   );
 
   startGame(theBoard);
+
+  int a = 1, user = 2, tmp;
+
+  while (true) {
+    tmp = a;
+    a = user;
+    user = tmp;
+    currentBoard(theBoard);
+
+    stdout.write("Please user $user, choose a coordinate : ");
+    List userChoice = stdin.readLineSync()!.split(" ");
+
+    if (userChoice.join(" ") == "exit") {
+      print("\nGame quitted int the following state :");
+      break;
+    }
+  }
 }

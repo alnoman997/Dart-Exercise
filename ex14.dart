c68073ccc5b3633b3a6d872e8f99a31e04f76a96
alnoman997 <alnoman997@gmail.com>
2026-05-02 06:18:29 +0600

    remove duplicates from list

---
+++ ex14.dart
@@ -0,0 +1,12 @@
+// Write a program (function) that takes a list and returns a new list
+// that contains all the elements of the first list minus all the duplicates.
+
+import 'dart:math';
+
+void main() {
+  final random = Random();
+  List<int> randList = List.generate(10, (_) => random.nextInt(10));
+
+  print("Initial list is $randList");
+  print("Cleaned list is ${removeDuplicates(randList)}");
+}

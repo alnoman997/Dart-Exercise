00e2a61f41128ed0416e97e9358067148ab33c72
alnoman997 <alnoman997@gmail.com>
2026-05-02 07:57:30 +0600

    draw gameboard

---
+++ ex17.dart
@@ -0,0 +1,22 @@
+//  --- --- ---
+// |   |   |   |
+//  --- --- ---
+// |   |   |   |
+//  --- --- ---
+// |   |   |   |
+//  --- --- ---
+
+//  This one is 3x3 (like in tic tac toe).
+
+// Ask the user what size game board they want to draw,
+// and draw it for them to the screen using Dart’s print statement.
+
+import 'dart:io';
+
+void main() {
+  stdout.write("What square size do you want? ");
+  int userChoice = int.parse(stdin.readLineSync() ?? '0');
+  print("Here is your ${userChoice} by ${userChoice} game board:\n");
+
+  drawBoard(userChoice);
+}

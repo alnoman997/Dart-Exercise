1cf4f4d09829ebb2f48164c9d1abf6c7189d0b9d
alnoman997 <alnoman997@gmail.com>
2026-05-03 19:59:49 +0600

    guessing number

--- ex21.dart
+++ ex21.dart
@@ -1,3 +1,17 @@
 // You, the user, will have in your head a number between 0 and 100.
 // The program will guess a number, and you, the user, will say
 // whether it is too high, too low, or your number.
+
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print("""\n
+  Hello! I am your laptop.
+  Please think of a number between 0 and 100, and I will try to guess it.
+  
+  If my guess is too low, type "low". If I am too high, type "high". If I am correct, type "correct".
+  """);
+
+  compGuess();
+}
